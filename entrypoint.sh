72ad412c0edb23b17766736de9bd10d6ab55cbb2
Megamia <luudat214@gmail.com>
2025-07-05 17:35:21 +0700

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-echo "🚀 Starting application..."
-echo "🌐 PORT is: ${PORT:-8080}"
 
-php artisan serve --host=0.0.0.0 --port=${PORT:-8080}
+echo "📢 PORT is: ${PORT:-8080}"
+echo "🌐 Application running at http://0.0.0.0:${PORT:-8080}"
+
+php artisan serve --host=0.0.0.0 --port=${PORT:-8080} -v
+
