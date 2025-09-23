a50875d68ceb4e9e81f1c14c3678c4d77473991c
Megamia <luudat214@gmail.com>
2025-09-23 09:06:53 +0700

    update

--- plugins/vdomah/jwtauth/routes.php
+++ plugins/vdomah/jwtauth/routes.php
@@ -57,6 +57,7 @@ Route::group(['prefix' => 'api'], function () {
             value: $token,
             minutes: 1440,
             path: '/',
+            domain: '.banthuoclive-fe.vercel.app',
             sameSite: 'None',
             secure: $secure,
             httpOnly: true,
